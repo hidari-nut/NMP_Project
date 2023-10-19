e681d4a95b0817ee8703c2628795f29bc2de14bf
hidari-nut <madevis02@gmail.com>
2023-10-19 18:48:05 +0700

    all

--- app/src/main/java/com/viswa/nmp_cerbung_goofy_goober/HomeActivity.kt
+++ app/src/main/java/com/viswa/nmp_cerbung_goofy_goober/HomeActivity.kt
@@ -1,10 +1,15 @@
 package com.viswa.nmp_cerbung_goofy_goober
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.viswa.nmp_cerbung_goofy_goober.databinding.ActivityHomeBinding
+import com.viswa.nmp_cerbung_goofy_goober.databinding.ActivityLogInBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
 
     var recyclerView: RecyclerView? = null
     var recyclerViewCerbung: RecyclerViewCerbung? = null
@@ -12,7 +17,9 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         cerbungList.addAll(Global.cerbungs) // Add Cerbungs from Global
 
@@ -23,5 +30,10 @@ class HomeActivity : AppCompatActivity() {
         recyclerView?.adapter = recyclerViewCerbung
 
         recyclerViewCerbung?.notifyDataSetChanged()
+
+        binding.buttonAddCerbung.setOnClickListener{
+            val intent = Intent(this, CreateCerbungsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/viswa/nmp_cerbung_goofy_goober/LogInActivity.kt
+++ app/src/main/java/com/viswa/nmp_cerbung_goofy_goober/LogInActivity.kt
@@ -21,7 +21,7 @@ class LogInActivity : AppCompatActivity() {
             val enteredUsername = binding.inputUname.text.toString()
             val enteredPassword = binding.inputPwd.text.toString()
 
-            Toast.makeText(this, "blablabla", Toast.LENGTH_SHORT).show()
+            // Toast.makeText(this, "blablabla", Toast.LENGTH_SHORT).show()
             if (enteredUsername == username && enteredPassword == password) {
                 Toast.makeText(this, "Correct username and password!", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, HomeActivity::class.java)
