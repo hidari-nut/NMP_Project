a8c117c12f5f71c23efde914b514e36a23b61761
DHSNIEL <sukidi.dh@gmail.com>
2024-01-04 13:53:12 +0700

    added the layout for notifications and respond activity

---
+++ app/src/main/java/com/viswa/nmp_cerbung_goofy_goober/fragment_respond.kt
@@ -0,0 +1,11 @@
+package com.viswa.nmp_cerbung_goofy_goober
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class fragment_respond : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_respond)
+    }
+}
